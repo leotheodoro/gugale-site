9cb03b701addc2eff1ef3d601dafa583b042385b
Leonardo Theodoro <simplespinheiro@gmail.com>
2020-06-15 13:56:21 -0300

    removing some unused files

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,4 @@
 import React from "react";
-import tw from "twin.macro"; //eslint-disable-line
-import { css } from "styled-components/macro"; //eslint-disable-line
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 
 import Hero from "components/hero/TwoColumnWithVideo.js";
